1f4e82a96a05b7b2e0c4670557d9564d310043d7
Sanka Udeshika <sankaudeshika123@gmail.com>
2024-03-29 19:50:35 +0530

    Done Programme Update Process and Refresh Process

--- src/GUI/AdminManageTeachers.java
+++ src/GUI/AdminManageTeachers.java
@@ -7,6 +7,7 @@ package GUI;
 import Connection.MySQL;
 import java.sql.ResultSet;
 import java.util.Vector;
+import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
 /**
@@ -20,15 +21,24 @@ public class AdminManageTeachers extends javax.swing.JFrame {
      */
     public AdminManageTeachers() {
         initComponents();
+        setLocationRelativeTo(null);
         RefreshTable();
     }
-
+    
+    public void RefreshTextfield() {
+        jTextField1.setText("");
+        jTextField2.setText("");
+        jTextField3.setText("");
+        jTextField5.setText("");
+        jTextField4.setText("");
+    }
+    
     public void RefreshTable() {
         try {
             ResultSet rs = MySQL.execute("SELECT * FROM `teacher`");
             DefaultTableModel tb = (DefaultTableModel) jTable1.getModel();
             tb.setRowCount(0);
-
+            
             while (rs.next()) {
                 Vector v = new Vector();
                 v.add(rs.getString(1));
@@ -67,6 +77,7 @@ public class AdminManageTeachers extends javax.swing.JFrame {
         jLabel5 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
         jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -102,6 +113,11 @@ public class AdminManageTeachers extends javax.swing.JFrame {
         });
 
         jButton1.setText("Update");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton2.setText("Delete");
 
@@ -126,6 +142,13 @@ public class AdminManageTeachers extends javax.swing.JFrame {
             }
         });
 
+        jButton4.setText("Refresh ");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -135,23 +158,21 @@ public class AdminManageTeachers extends javax.swing.JFrame {
                 .addComponent(jLabel1)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                        .addGroup(layout.createSequentialGroup()
-                            .addContainerGap()
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                .addGroup(layout.createSequentialGroup()
-                                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addGap(7, 7, 7))
-                                .addComponent(jToggleButton1, javax.swing.GroupLayout.Alignment.TRAILING)))
-                        .addGroup(layout.createSequentialGroup()
-                            .addContainerGap()
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jLabel2)))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(7, 7, 7))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addComponent(jButton4)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jToggleButton1)))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                    .addComponent(jLabel2))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 452, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -187,7 +208,9 @@ public class AdminManageTeachers extends javax.swing.JFrame {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jToggleButton1)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jToggleButton1)
+                            .addComponent(jButton4))
                         .addGap(45, 45, 45)
                         .addComponent(jButton1)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -225,12 +248,12 @@ public class AdminManageTeachers extends javax.swing.JFrame {
     private void jToggleButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleButton1ActionPerformed
         // Search Teacher by Email:
         String Name = jTextField1.getText();
-
+        
         try {
             ResultSet rs = MySQL.execute("SELECT * FROM `teacher` WHERE `Fname` LIKE '%" + Name + "%' ");
             DefaultTableModel tb = (DefaultTableModel) jTable1.getModel();
             tb.setRowCount(0);
-
+            
             while (rs.next()) {
                 Vector v = new Vector();
                 v.add(rs.getString(1));
@@ -259,14 +282,46 @@ public class AdminManageTeachers extends javax.swing.JFrame {
         String FirstName = String.valueOf(jTable1.getValueAt(SelectRow, 1));
         String LastName = String.valueOf(jTable1.getValueAt(SelectRow, 2));
         String Mobile = String.valueOf(jTable1.getValueAt(SelectRow, 3));
-
+        
         jTextField2.setText(Email);
+        jTextField2.setEditable(false);
         jTextField3.setText(FirstName);
         jTextField5.setText(LastName);
         jTextField4.setText(Mobile);
-
+        
 
     }//GEN-LAST:event_jTable1MouseClicked
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // Update Details  Process
+        String Email = jTextField2.getText();
+        String Fname = jTextField3.getText();
+        String Lname = jTextField5.getText();
+        String Mobile = jTextField4.getText();
+        
+        if (Email.isEmpty()) {
+            JOptionPane.showMessageDialog(this, "Please Enter a Email Address");
+        } else if (Fname.isEmpty()) {
+            JOptionPane.showMessageDialog(this, "Please Enter a First Name");
+        } else if (Lname.isEmpty()) {
+            JOptionPane.showMessageDialog(this, "Please Enter a Last Name");
+        } else if (Mobile.isEmpty()) {
+            JOptionPane.showMessageDialog(this, "Please Enter a Mobile");
+        } else {
+            MySQL.execute("UPDATE `teacher` SET `Fname` = '" + Fname + "' , `Lname` = '" + Lname + "' , `Mobile` = '" + Mobile + "' WHERE `Email` = '" + Email + "' ");
+            JOptionPane.showMessageDialog(this, "Update Success");
+            RefreshTable();
+            RefreshTextfield();
+        }
+        
+
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // Reffresh the Page 
+        RefreshTable();
+        RefreshTextfield();
+    }//GEN-LAST:event_jButton4ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -307,6 +362,7 @@ public class AdminManageTeachers extends javax.swing.JFrame {
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
